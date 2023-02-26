89bbd1b89a8f7e8428bd40d1d8bab5ca26255ebe
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-26 18:08:37 +0530

    creating header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,15 @@
+import "./Header.css";
 
 const Header = () => {
-
-    return (
-        <div></div>
-    );
+  return (
+    <div className="header">
+        <a>HOME</a>
+        <a>EXPERINCE</a>
+        <a>ABOUT</a>
+        <a>BLOGS</a>
+        <a>CONTACT</a>
+    </div>
+  );
 };
 
 export default Header;
