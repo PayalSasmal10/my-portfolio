734428312bf440967cee94950171c2660af37e02
Payal Sasmal <sasmalpayal@gmail.com>
2023-02-27 21:09:45 +0530

    fixing the css of Home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,34 +1,40 @@
 import { AiFillLinkedin } from 'react-icons/ai';
 import { FaGithubSquare, FaTwitterSquare } from 'react-icons/fa';
+import './Home.css';
 
 
 const Home = () => {
   return (
-    <section>
+    <section className='container intro_body'>
       <div>
         <div>
           <div>
             <div>
-              <img src="/assets/profile.jpg" alt="" />
+              <img src="./assets/profile.jpg" alt="" />
             </div>
           </div>
         </div>
       </div>
-      <div>
-        <div>Hello! I'm</div>
+      <div className='introHolder'>
+        <div className='intro'>Hello! I'm</div>
         <div>Payal Sasmal</div>
         <div>Software Engineer</div>
       </div>
       <div>
         <div>
-            <a href="">
+            <a href="https://www.linkedin.com/in/payalsasmal/">
                 <div><AiFillLinkedin/></div>
             </a>
-            <a href="">
+            <a href="https://github.com/PayalSasmal10">
                 <div><FaGithubSquare/></div>
             </a>
-            <a href="">
+            <a href="https://twitter.com/payalsasmal">
                 <div><FaTwitterSquare/></div>
+            </a>
+            <a href="">
+                <div>
+                    <img src='https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/dev-dot-to.svg'/>
+                </div>
             </a>
         </div>
       </div>
