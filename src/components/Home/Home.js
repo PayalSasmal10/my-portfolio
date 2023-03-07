677691b43911dc4f889167061d179cc8219182f4
Payal Sasmal <sasmalpayal@gmail.com>
2023-03-07 22:38:59 +0530

    fixing the home css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,44 +1,57 @@
-import { AiFillLinkedin } from 'react-icons/ai';
-import { FaGithubSquare, FaTwitterSquare } from 'react-icons/fa';
-import './Home.css';
+import { AiFillLinkedin } from "react-icons/ai";
+import { FaGithubSquare, FaTwitterSquare } from "react-icons/fa";
+import "./Home.css";
 import loading_img from "../../assets/loading-background.gif";
 
 const Home = () => {
   return (
-    <section className='container intro_body' id='home'>
-      <div data-aos="zoom-in" className='intro_profile_pic_holder aos-init aos-animate'>
-        <div className='intro_Ring intro_Ring1'>
-          <div className='intro_Ring intro_Ring2'>
-            <div className='intro_Ring intro_Ring3'>
-              <span alt="profile_pic" style={{backgroundImage: 'url(loading_img)'}} />
+    <main>
+      <section className="container intro_body" id="home">
+        <div
+          data-aos="zoom-in"
+          className="intro_profile_pic_holder aos-init aos-animate">
+          <div className="intro_Ring intro_Ring1">
+            <div className="intro_Ring intro_Ring2">
+              <div className="intro_Ring intro_Ring3">
+                <span
+                  alt="profile_pic"
+                  style={{ backgroundImage: "url(loading_img)" }}
+                />
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='introHolder'>
-        <div className='intro'>Hello! I'm</div>
-        <div className='intro_name'>Payal Sasmal</div>
-        <div className='intro_designation'>Software Engineer</div>
-      </div>
-      <div className='intro_ContactContainer'>
-        <div className='intro_ContactLinkHolder'>
+        <div data-aos="fade-left" className="introHolder aos-init aos-animate">
+          <div className="intro">Hello! I'm</div>
+          <div className="intro_name">Payal Sasmal</div>
+          <div className="intro_designation">Software Engineer</div>
+        </div>
+        <div className="intro_ContactContainer">
+          <div className="intro_ContactLinkHolder">
             <a href="https://www.linkedin.com/in/payalsasmal/">
-                <div><AiFillLinkedin/></div>
+              <div>
+                <AiFillLinkedin />
+              </div>
             </a>
             <a href="https://github.com/PayalSasmal10">
-                <div><FaGithubSquare/></div>
+              <div>
+                <FaGithubSquare />
+              </div>
             </a>
             <a href="https://twitter.com/payalsasmal">
-                <div><FaTwitterSquare/></div>
+              <div>
+                <FaTwitterSquare />
+              </div>
             </a>
             <a href="">
-                <div>
-                    <img src='https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/dev-dot-to.svg'/>
-                </div>
+              <div>
+                <img src="https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/dev-dot-to.svg" />
+              </div>
             </a>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </main>
   );
 };
 
