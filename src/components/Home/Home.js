f13c11a512a1fcc988622c1a9a786fdd9b18abce
Payalsasmal10 <sasmalpayal@gmail.com>
2024-04-07 21:03:40 +0530

    working with css

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
   return (
     <main>
       <section className="container intro_body" id="home">
-        <div
+        {/* <div
           data-aos="zoom-in"
           className="intro_profile_pic_holder aos-init aos-animate">
           <div className="intro_Ring intro_Ring1">
@@ -20,35 +20,35 @@ const Home = () => {
               </div>
             </div>
           </div>
-        </div>
-        <div data-aos="fade-left" className="introHolder aos-init aos-animate">
+        </div> */}
+        <div className="introHolder">
           <div className="intro">Hello! I'm</div>
           <div className="intro_name">Payal Sasmal</div>
-          <div className="intro_designation">Software Engineer</div>
+          <div className="intro_designation">Senior Software Engineer</div>
         </div>
         <div className="intro_ContactContainer">
-          <div className="intro_ContactLinkHolder">
+          {/* <div className="intro_ContactLinkHolder"> */}
             <a href="https://www.linkedin.com/in/payalsasmal/" target="_blank" rel="noreferrer">
               <div>
                 <AiFillLinkedin />
               </div>
             </a>
-            <a href="https://github.com/PayalSasmal10">
+            <a href="https://github.com/PayalSasmal10" target="_blank" rel="nopener noreferrer">
               <div>
                 <FaGithubSquare />
               </div>
             </a>
-            <a href="https://twitter.com/payalsasmal" target="_blank">
+            <a href="https://twitter.com/payalsasmal" target="_blank" rel="noopener noreferrer">
               <div>
                 <FaTwitterSquare />
               </div>
             </a>
-            <a href="#">
+            <a href="https://dev.to/payalsasmal" target="_blank" rel="noopener noreferrer">
               <div>
-                <img src="https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/dev-dot-to.svg" />
+                <img src="https://cdn.jsdelivr.net/npm/simple-icons@v4/icons/dev-dot-to.svg" alt="payalsasmal dev.to" />
               </div>
             </a>
-          </div>
+          {/* </div> */}
         </div>
       </section>
     </main>
