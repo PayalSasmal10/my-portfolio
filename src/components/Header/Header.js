07fe54c4a8ecddeec9c646335163b61568634604
Payalsasmal10 <sasmalpayal@gmail.com>
2024-04-09 22:44:02 +0530

    Making Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,15 +2,20 @@ import "./Header.css";
 
 const Header = () => {
   return (
-    <div className="header">
-      <div  className="headerA">
+    <nav className="style-nav">
+
+    <div className="name-details">
+      {/* <div  className="headerA">
         <a href="#" className="headerProperties">Home</a>
         <a href="#" className="headerProperties">Experience</a>
         <a href="#" className="headerProperties">About</a>
         <a href="#" className="headerProperties">Blogs</a>
         <a href="#" className="headerProperties">Contact</a>
-      </div>
+      </div> */}
+      <span>PS</span>
     </div>
+    <div></div>
+    </nav>
   );
 };
 
